d6745ad697b05c11b2ae56ca34f98783f2dfc5c8
BlackDontWhite <nhanb1910270@student.ctu.edu.vn>
2023-04-16 20:16:41 +0700

    Cap nhat, xoa san pham tren Realtime Database cua Firebase

--- lib/ui/products/products_manager.dart
+++ lib/ui/products/products_manager.dart
@@ -72,11 +72,13 @@ class ProductsManager with ChangeNotifier{
     }
   }
 
-  void updateProduct(Product product){
+  Future<void> updateProduct(Product product) async {
     final index = _items.indexWhere((item) => item.id == product.id);
     if(index >= 0){
-      _items[index] = product;
-      notifyListeners();
+      if(await _productsSevice.updateProduct(product)){
+        _items[index] = product;
+        notifyListeners();
+      }
     }
   }
 
@@ -85,10 +87,16 @@ class ProductsManager with ChangeNotifier{
     product.isFavorite = !saveStatus;
   }
 
-  void deleteProduct(String id){
+  Future<void> deleteProduct(String id) async {
     final index = _items.indexWhere((item) => item.id == id);
+    Product? existingProduct = _items[index];
     _items.removeAt(index);
     notifyListeners();
+
+    if(!await _productsSevice.deleteProduct(id)){
+      _items.insert(index, existingProduct);
+      notifyListeners();
+    }
   }
 
   final ProductsService _productsSevice;
